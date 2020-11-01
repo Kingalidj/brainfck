182e321c0e5508108d4d9d5ee6f94026f3ab0da6
Kingalidj <44316365+Kingalidj@users.noreply.github.com>
2020-11-01 18:29:04 +0100

    less lines

--- brainfck.py
+++ brainfck.py
@@ -1,92 +1,39 @@
-from time import sleep
-import re
-stack = [0]
-
+from time import sleep; from re import sub
 def readBF (name):
-    code = ""
-    with open(name, 'r') as f:
-        for line in f:
-            code += line.strip()
-    return re.sub("[^\[\]+-.,<>]", "", code)
+    with open(name, 'r') as f: return sub("[^\[\]+-.,<>]", "", f.read())
 
 def printStack(stack, pointer = -1):
-    for i, s in enumerate(stack):
-        print(f"[{s}]" if i == pointer else s, end = " ")
+    for i, s in enumerate(stack): print(f"[{s}]" if i == pointer else s, end = " ")
     print()
 
-def getMatch(code, pos, forward = True):
+def getMatch(code, pos, fwd = True):
+    if (pos < 0 or pos >= len(code)): print("Array index out of range: " + str(pos)); return
+    if (code[pos] != "[" and fwd or code[pos] != "]" and not fwd): print(f"Wrong starting position: code[{pos}] = {code[pos]}"); return 
     openBr = 1
-    if (pos < 0 or pos >= len(code)):
-        print("Array index out of range: " + str(pos))
-        return
+    x = 1 if fwd else -1
+    for i in range(pos + x, len(code) if fwd else x, x):
+        if code[i] == "[": openBr += 1 * x
+        if code[i] == "]": openBr -= 1 * x
+        if openBr == 0: return i - pos
 
-    if forward:
-        if (code[pos] != "["):
-            print("Wrong starting position: " + str(pos))
-            return 
-
-        for i in range(pos + 1, len(code)):
-            if code[i] == "[":
-                openBr += 1
-            if code[i] == "]":
-                openBr -= 1
-            if openBr == 0:
-                return i - pos
-    else:
-        if (code[pos] != "]"):
-            print("Wrong starting position: " + str(pos))
-            return 
-
-        for i in range(pos - 1, -1, -1):
-            if code[i] == "[":
-                openBr -= 1
-            if code[i] == "]":
-                openBr += 1
-            if openBr == 0:
-                return i - pos
-
-def getSubCode(code, pos):
-    return code[pos + 1 : getMatch(code, pos) + pos]
-
-def compute(code, show = False):
-    global stack
-    pointer = 0
-    output = ""
-    offset = 0
-
-    i = 0
-    while (i + offset < len(code)):
-        c = code[i + offset]
-        if c == '>':
+def compute(code, show = False, timeStep = 0):
+    stack = [0]; pointer = 0; output = ""; i = 0
+    while (i < len(code)):
+        c = code[i]
+        if   c == '>': 
             pointer += 1
-            if (pointer >= len(stack)):
-                stack.append(0)
-        if c == '<':
-            pointer -= 1
-            if (pointer < 0):
-                print("Memory error: -1")
-        if c == '+':
-            stack[pointer] += 1
-        if c == '-':
-            stack[pointer] -= 1
-            if stack[pointer] < 0: stack[pointer] = 255
-        if c =='.':
-            output += chr(stack[pointer])
-            #print(f"Output: {output}")
-        if c ==',':
-            stack[pointer] = ord(input("Input: "))
-        if c == ']':
-            if stack[pointer] != 0:
-                offset += getMatch(code, i + offset, False)
-        if c == '[':
-            if stack[pointer] == 0:
-                 offset += getMatch(code, i + offset)
-        
-        if (show):
-            printStack(stack, pointer)
-            #sleep(0.1)
+            if (pointer >= len(stack)): stack.append(0)
+        elif c == '<':
+            if (pointer < 0): print("Memory error: -1")
+            else: pointer -= 1
+        elif c == '+': stack[pointer] = stack[pointer] + 1 if (stack[pointer] <= 255 ) else 0
+        elif c == '-': stack[pointer] = stack[pointer] - 1 if (stack[pointer] > 0) else 255
+        elif c == '.': output += chr(stack[pointer]); print(chr(stack[pointer]), end = '')
+        elif c == ',': stack[pointer] = ord(input("Input: "))
+        elif c == ']' and stack[pointer] != 0: i += getMatch(code, i, False)
+        elif c == '[' and not stack[pointer]: i += getMatch(code, i)
+        if (show): printStack("\n" + stack, pointer); sleep(timeStep)
         i += 1
-
     print(f"Output: {output}")
 
 code = readBF("test.bf")
